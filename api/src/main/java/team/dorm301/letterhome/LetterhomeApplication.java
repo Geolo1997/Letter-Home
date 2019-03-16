f36a87a4ebbfc015d4ac6198820afb91e95b4019
zhangxishuo <zhangxishuo1998@gmail.com>
2019-03-16 15:26:42 +0800

    api

---
+++ api/src/main/java/team/dorm301/letterhome/LetterhomeApplication.java
@@ -0,0 +1,13 @@
+package team.dorm301.letterhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterhomeApplication.class, args);
+	}
+
+}
