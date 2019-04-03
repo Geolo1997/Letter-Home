8ccd90a6bafd2907bad3132382ed713a4ec33779
zhangxishuo <zhangxishuo1998@gmail.com>
2019-04-03 16:08:11 +0800

    发信接口

--- api/src/main/java/team/dorm301/letterhome/controller/LetterController.java
+++ api/src/main/java/team/dorm301/letterhome/controller/LetterController.java
@@ -1,4 +1,24 @@
 package team.dorm301.letterhome.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.dorm301.letterhome.entity.Letter;
+import team.dorm301.letterhome.service.LetterService;
+
+@RestController
+@RequestMapping("letter")
 public class LetterController {
+
+    private final LetterService letterService;
+
+    public LetterController(LetterService letterService) {
+        this.letterService = letterService;
+    }
+
+    @PostMapping
+    public void send(@RequestBody Letter letter) {
+        letterService.send(letter);
+    }
 }
